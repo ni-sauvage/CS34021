086bb56c382bf7190e5c844af805ef7be9bfb563
ni-sauvage <sauvagen@tcd.ie>
2022-11-08 13:41:43 +0000

    Optimised Assign3_ex1.asm

--- Assign3_ex1.asm
+++ Assign3_ex1.asm
@@ -16,13 +16,11 @@ else_label:
 max4:
     add r26, r0, r10             ; Set up first call by moving r26 into r10 (first param == a)
     add r27, r0, r11             ; Set up first call by moving r27 into r11 (second param == b)
-    add r28, r0, r12             ; Set up first call by moving r28 into r12 (third param == c)
     callr max3, r15              ; Call max3 with params a, b, c
-    xor r0,  r0, r0              ; NOP operation, delay while call being calculated
+    add r28, r0, r12             ; Set up first call by moving r28 into r12 (third param == c)
     add r29, r0, r12             ; Set up second call by moving r29 into r12 (third param == d)
-    add r1,  r0, r10             ; Set up second call by moving result of first call into r10 (first call)
     callr max3, r15              ; Call max3 with params max3(a, b, c), c, d. c has not moved from r11, so we do not need to load it again.
-    xor r0,  r0, r0              ; NOP operation, delay while call being calculated
+    add r1,  r0, r10             ; Set up second call by moving result of first call into r10 (first call)
     ret r31(0)                   ; Return the result of the second call
     xor r0,  r0, r0              ; NOP operation, delay while return being calculated
 
@@ -30,9 +28,8 @@ main:
     add r0, #100, r10           ; Set up call to max4 by moving 100 into r10 (first param == 100)
     add r0, #99,  r11           ; Set up call to max4 by moving 99 into r11 (first param == 99)
     add r0, #43,  r12           ; Set up call to max4 by moving 43 into r12 (first param == 43)
-    add r0, #111, r13           ; Set up call to max4 by moving 111 into r13 (first param == 111)
     callr max4, r15             ; Call max4 with params 100, 99, 43 and 111
-    xor r0, r0,   r0            ; NOP operation, delay while call being calculated
+    add r0, #111, r13           ; Set up call to max4 by moving 111 into r13 (first param == 111)
     add r0, r0,   r1            ; Move 0 into r1
     ret r31(0)                  ; return 0 
     xor r0,  r0, r0             ; NOP operation, delay while return being calculated
